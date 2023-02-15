a1767f28baf2bee92b2dc7dc474b7372924101a0
mrman888 <amiibospam@gmail.com>
2023-02-15 20:28:21 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello222{{value}}</div>`,
 })
 export class AppComponent {
   value = 'World';
